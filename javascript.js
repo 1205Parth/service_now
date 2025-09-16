159c5aca92919ac76f5ae50899f6eaec6b70fd9f
Parth Vaghasiya <162993263+1205Parth@users.noreply.github.com>
2025-09-16 12:27:34 +0530

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,7 @@
+// Simple JavaScript Demo
+
+// Print message to the browser console
+console.log("Hello, World!");
+
+// Show an alert pop-up with greeting
+alert("Welcome to your JavaScript demo!");
